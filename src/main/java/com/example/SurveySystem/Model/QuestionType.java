65571c2e7a05857bea843ef2a62618dd19006700
Daurbek Sakhtarov <61458120+daurbekSD@users.noreply.github.com>
2022-05-21 08:08:10 +0600

    Models created

--- src/main/java/com/example/SurveySystem/Model/QuestionType.java
+++ src/main/java/com/example/SurveySystem/Model/QuestionType.java
@@ -1,21 +1,2 @@
-package com.example.SurveySystem.Model;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-@Entity(name = "questionType")
-@Table
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class QuestionType {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @Column(name = "type")
-    private String type;
+package com.example.SurveySystem.Model;public enum QuestionTypeEnum {
 }
